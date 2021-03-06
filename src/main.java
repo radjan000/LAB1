d33fddc4f759021b28abfcaafbe2ecf647d45166
radjan000 <radjan000@utp.edu.pl>
2021-03-06 15:27:22 +0100

    LAB1_v9

--- src/main.java
+++ src/main.java
@@ -1,7 +1,6 @@
 public class main {
     public static void main(String[] args) {
-        //Autor: Radosław Janka
-        //Autor: Marcin Rospara DORIAN BORZYM
+
 
         // NAZWA PROJEKTU 1
     }
